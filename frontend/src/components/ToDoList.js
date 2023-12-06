5cf90aa126c0a0e76057385b153ffe764adbb9bc
sagar sangwan <iamsagarsangwan@gmail.com>
2023-12-07 00:35:19 +0530

    data fetch from backend done and showed on frontend

--- frontend/src/components/ToDoList.js
+++ frontend/src/components/ToDoList.js
@@ -2,12 +2,14 @@ import React from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
 
 
-export default function ToDoList() {
+export default function ToDoList({ todos = [] }) {
     return (<ListGroup>
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-        <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        {todos.map(
+            todo => {
+
+                return <ListGroup.Item key={todo.id}>{todo.task}</ListGroup.Item>
+            }
+        )}
+
     </ListGroup>)
 }
--- toDoList/todoapp/urls.py
+++ toDoList/todoapp/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from .views import todo_list, todo_detail
 
 
-urlpatterns = [path("todos", todo_list), path("todos/<int:pk>", todo_detail)]
+urlpatterns = [path("todos/", todo_list), path("todos/<int:pk>", todo_detail)]

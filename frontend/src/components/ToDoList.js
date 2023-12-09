057ea84d834c1df43d4f3b5c7ab1725eddc521ff
sagar sangwan <iamsagarsangwan@gmail.com>
2023-12-09 23:17:30 +0530

    update todo is now working

--- frontend/src/components/ToDoList.js
+++ frontend/src/components/ToDoList.js
@@ -1,13 +1,49 @@
 import axios from "axios";
 import React, { useState } from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
-import { MdCheckBox, MdCheckBoxOutlineBlank, MdEdit, MdDelete } from "react-icons/md"
+import { MdEdit, MdDelete } from "react-icons/md"
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl"
 
 
 export default function ToDoList({ todos, setTodos, filterToDo }) {
     const [todo, setToDo] = useState({})
+    const [show, setShow] = useState(false);
 
+    const [todOformvalue, setToDOFormValue] = useState("");
+    const handelchange = e => {
+        setToDOFormValue(e.target.value);
+    };
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const handleUpdateToDo = (e, todo) => {
+        setShow(true);
+        setToDo(todo);
+        setToDOFormValue(todo.task)
+        const updateToDO = { ...todo, task: todOformvalue }
+        console.log(updateToDO)
+
+        // axios.patch(`http://localhost:8000/todos/${todo.id}`, updateToDO)
+
+    }
+    // handle update of form
+    const handleUpdateFormTask = e => {
+        e.preventDefault();
+        const updateToDO = { ...todo, task: todOformvalue }
+        axios.patch(`http://localhost:8000/todos/${todo.id}`, updateToDO)
+            .then((res) => {
+                console.log(res)
+                handleClose()
+            }, [])
+
+    }
+
+
+    // for deletiomn of todo
     const handleDelete = (id) => {
         const originalTodo = [...todos]
         setTodos(todos.filter(todo => todo.id !== id))
@@ -21,17 +57,16 @@ export default function ToDoList({ todos, setTodos, filterToDo }) {
             )
 
     }
+
+    // for marking todo ad completed or not
     const handleCompletedOrNot = (e, id, todo) => {
         if (e.target.checked) {
             console.log("checked");
             setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: true } : todo))
             const updateToDO = { ...todo, completed: true }
             console.log(updateToDO)
+
             axios.patch(`http://localhost:8000/todos/${id}`, updateToDO)
-
-
-
-
 
         }
         else {
@@ -41,7 +76,9 @@ export default function ToDoList({ todos, setTodos, filterToDo }) {
 
     }
     const completedToDos = todos.filter(todo => todo.completed);
-    const unCompletedToDo = todos.filter(todo => todo.completed == false);
+    const unCompletedToDo = todos.filter(todo => todo.completed === false);
+
+    // listgrop body
     const renderListGroupItem = (todo) => {
         return <ListGroup.Item key={todo.id} className="d-flex justify-content-center align-items-center">
             <div className="d-flex justify-content-center" >
@@ -51,7 +88,7 @@ export default function ToDoList({ todos, setTodos, filterToDo }) {
                 <span style={todo.completed ? { textDecoration: 'line-through', } : {}}>{todo.task} {todo.id}</span>
             </div>
             <div >
-                <MdEdit style={{ marginRight: "12px", cursor: "pointer" }} />
+                <MdEdit style={{ marginRight: "12px", cursor: "pointer" }} onClick={(e) => handleUpdateToDo(e, todo)} />
                 <MdDelete onClick={() => handleDelete(todo.id)} style={{ marginRight: "12px", cursor: "pointer" }} />
             </div>
 
@@ -99,5 +136,29 @@ export default function ToDoList({ todos, setTodos, filterToDo }) {
                 </ListGroup>
             )
             }
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal title</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={handleUpdateFormTask}>
+                        <InputGroup className='mb-4'>
+                            <FormControl value={todOformvalue} onChange={handelchange}
+                            />
+                            <Button type='submit'>Add</Button>
+                        </InputGroup>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+
+                </Modal.Footer>
+            </Modal>
         </div>)
 }
